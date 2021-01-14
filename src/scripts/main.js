935838d060b545cd9a225ea39ba7fd29424e851b
christinaashworth <christina.ashworth@ymail.com>
2021-01-14 10:31:14 -0600

    setting up database

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,3 +10,4 @@ import { Nutshell } from "./Nutshell.js"
     4. Also, if the user authenticates, and the login form is initially shown
         ensure that the Nutshell component gets rendered
 */
+
