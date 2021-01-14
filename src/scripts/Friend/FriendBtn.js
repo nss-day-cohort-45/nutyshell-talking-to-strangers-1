de052fbe422c1f19553279ccc198b7c3d1e0a0f0
christinaashworth <christina.ashworth@ymail.com>
2021-01-14 14:25:41 -0600

    pulling down main

--- src/scripts/Friend/FriendBtn.js
+++ src/scripts/Friend/FriendBtn.js
@@ -1,2 +1,16 @@
 // Make "view friends" button and place it in header of the DOM.
 // listen for click event and open dialog box&call list function upon click
+
+const contentTarget = document.querySelector("#viewFriendsBtn")
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "showNotes") {
+        const customEvent = new CustomEvent("showNotesClicked")
+        eventHub.dispatchEvent(customEvent)
+    }
+})
+
+export const ShowNoteButton = () => {
+    contentTarget.innerHTML = "<button id='showNotes'>Show Notes</button>"
+}
