feaf5100ed59a24d82d34812282bdab4e4c275c8
anna-moore <ammoore434@gmail.com>
2021-01-15 11:38:21 -0600

    message HTML converter

--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -3,6 +3,7 @@ import { NewsList } from "./News/NewsList.js";
 import { FriendForm } from "./Friend/FriendForm.js";
 import { EventList } from "./Events/EventList.js";
 import { EventForm } from "./Events/EventForm.js";
+import { MessageForm } from "./Message/MessageForm.js"
 
 export const Nutshell = () => {
     NewsList();
--- src/scripts/message/Message.js
+++ src/scripts/message/Message.js
@@ -1,2 +1,14 @@
 //Anna Moore
-//the goal of this module is 
+//the goal of this module is place json data of the messages into HTML format 
+
+//note that userId should be changed for username (Ron is currently working on this function)
+export const MessageHTMLconverter = (messageObj) => {
+    return `
+    <section class=""message id= > 
+        <h3 class="message__title"> Chat History </h3>
+        <div class="message__text" >${messageObj.message} </div>
+        <div class="message__time" >${messageObj.timestamp} </div>
+        <div class="message__userId" >${messageObj.userId} </div> 
+    </section>
+    `
+}
